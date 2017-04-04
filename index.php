ce274c17031f0a3bb558c3fdec4879b5f00c519b
Younes SADMI <younes.sadmi@gmail.com>
2017-04-04 16:12:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<DOCTYPE html>
+<html>
+    <html lang="en">
+        <?php include('head.php'); ?>
+    </html>
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">ESGI</a>
+                </div>
+            </div>
+        </nav>
+        <!-- Content -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                
+                </div>
+            </div>
+        </div>
+        <script src="js/jquery-3.2.0.min.js"></script>
+        <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+    </body>
+</html>
