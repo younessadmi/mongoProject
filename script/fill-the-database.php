02d0c472ee33359b3b675d4fa46eb2cfe892300c
Benjamin Hubert <benjam.hubert@gmail.com>
2017-04-06 21:43:30 +0200

    added comment

--- script/fill-the-database.php
+++ script/fill-the-database.php
@@ -7,9 +7,9 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 $content = $connection->get('account/verify_credentials');
 
+// BUILDING QUERY
 $showsJson = json_decode(file_get_contents('../json/shows.json'), true);
 $searchQuery = '';
-
 $i = 0;
 foreach($showsJson as $show){
     foreach($show as $s){
