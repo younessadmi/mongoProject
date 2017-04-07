da6b3990b24eafc65cece6e9e4abf46321de0898
Benjamin Hubert <benjam.hubert@gmail.com>
2017-04-07 10:27:36 +0200

    fixed conflict

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-    include('pre-requisite.php');
+include('pre-requisite.php');
 ?>
 
 <DOCTYPE html>
-<html>
     <html lang="en">
+    <head>
         <?php include('head.php'); ?>
-    </html>
+    </head>
     <body>
         <nav class="navbar navbar-default">
             <div class="container-fluid">
@@ -36,12 +36,30 @@
             </div>
         </nav>
         <div class="container">
-           <div class="row">
-               <div class="col-sm-12" style="text-align:right">
-                   <p class="label label-default" id="number-of-tweets"><?php echo getNumberOfTweets();?></p>
-                   <br><br><br>
-               </div>
-           </div>
+            <div class="row">
+                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab" id="headingOne">
+                            <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    En savoir plus sur le projet
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                            <div class="panel-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12" style="text-align:right">
+                    <p class="label label-default" id="number-of-tweets"><?php echo getNumberOfTweets();?></p>
+                    <br><br><br>
+                </div>
+            </div>
             <div class="row">
                 <!-- Graph -->
                  <div class="col-sm-12 col-md-6">
@@ -57,7 +75,7 @@
                         </div>
                      </div>
                  </div>
-               <!-- Graph -->
+                <!-- Graph -->
                 <div class="col-sm-12 col-md-6">
                     <div class="graph">
                         <div id="number-of-occurence-by-show">
@@ -67,7 +85,7 @@
                     </div>
                 </div>
                 <!-- Graph -->
-                 <div class="col-sm-12 col-md-6">
+                <div class="col-sm-12 col-md-6">
                     <div class="graph">
                         <div id="tweets-by-language">
                             <p class="graph-title">Tweets by language</p>
@@ -76,14 +94,13 @@
                      </div>
                  </div>
             </div>
-        </div>
-        <script src="js/jquery/jquery-3.2.0.min.js"></script>
-        <script src="js/bootstrap/bootstrap.min.js"></script>
-        <script src="js/nprogress/nprogress.js"></script>
-        <script src="https://code.highcharts.com/stock/highstock.js"></script>
-        <script src="https://code.highcharts.com/modules/exporting.js"></script>
-        <script src="https://code.highcharts.com/modules/no-data-to-display.js"></script>
-        
-        <script src="js/script.js"></script>
-    </body>
-</html>
+            <script src="js/jquery/jquery-3.2.0.min.js"></script>
+            <script src="js/bootstrap/bootstrap.min.js"></script>
+            <script src="js/nprogress/nprogress.js"></script>
+            <script src="https://code.highcharts.com/stock/highstock.js"></script>
+            <script src="https://code.highcharts.com/modules/exporting.js"></script>
+            <script src="https://code.highcharts.com/modules/no-data-to-display.js"></script>
+
+            <script src="js/script.js"></script>
+        </body>
+        </html>
